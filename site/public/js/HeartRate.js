6b5c6633be686f7773adf3cff9b489bf57285d37
angiemai <aweiruk@hotmail.com>
2013-10-08 19:07:08 +0100

    init

---
+++ site/public/js/HeartRate.js
@@ -0,0 +1,36 @@
+﻿
+
+jQuery(document).ready(function () {
+    alert("hi");
+    urlDataJSON = '~/Contact/HearthRateDataJSON';
+
+    $.getJSON(urlDataJSON, "", function (data) {
+        var dataLines = [];
+        var dataLabels = "";
+        $.each(data, function (entryindex, entry) {
+            dataLines.push(entry['Serie']);
+            dataLabels = dataLabels + entry['Name'];
+        });
+
+        Plot(dataLines, dataLabels);
+    });
+});
+
+function Plot(dataLines, dataLabels) {
+    var line1 = "{ label: 'line1.0' }";
+    options = {
+        legend: { show: true },
+        title: 'Heart rate overview',
+        axesDefaults: { pad: 1 },
+        seriesDefaults: { showMarker: false, trendline: { show: false }, lineWidth: 3 },
+        axes: {
+            yaxis: { min: 0, autoscale: true, label: 'HR[bpm]', labelRenderer: $.jqplot.CanvasAxisLabelRenderer },
+            xaxis: { autoscale: true, label: 'Time', labelRenderer: $.jqplot.CanvasAxisLabelRenderer }
+        }
+    };
+
+    //Data from database is already an array!
+    plot = $.jqplot('chartdiv', dataLines, options);
+
+    plot.redraw(); // gets rid of previous axis tick markers
+}
---
+++ site/public/js/appLines.js
@@ -0,0 +1,81 @@
+﻿
+var RocknCoder = RocknCoder || {};
+RocknCoder.Pages = RocknCoder.Pages || {};
+alert(line1);
+alert(line2);
+RocknCoder.Pages.Kernel = function (event) {
+	var that = this,
+		eventType = event.type,
+		pageName = $(this).attr("data-rockncoder-jspage");
+	pageName = "managePlotChart";
+	alert(pageName);
+		    RocknCoder.Pages["managePlotChart"]["pageshow"].call(this);
+};
+
+RocknCoder.Pages.Events = function () {
+    alert("RocknCoder.Pages.Events");
+	$("div[data-rockncoder-jspage]").on(
+		'pageshow',
+		RocknCoder.Pages.Kernel);
+} ();
+
+
+RocknCoder.Pages.managePlotChart = function () {
+    alert("dddd");
+	var pageshow = function () {
+			updateChart();
+		},
+		updateChart= function(){
+			showChart(line1, line2);
+		},
+		showChart = function (val1, val2) {
+		    alert(val1);
+		       $.jqplot('plotChart', [line1, line2], {
+		        title:'Blood Pressure Readings',
+		        axes:{
+		            xaxis:{
+		                renderer:$.jqplot.DateAxisRenderer,
+		                tickOptions:{
+		                     formatString:'%#d/%#m'
+    
+		},
+		                min: '23-May-08',
+                        tickInterval: '1 day'
+		            },
+		            yaxis:{
+		                tickOptions:{
+		                    formatString:'$%.2f'
+		                }
+		            }
+		        },
+		        highlighter: {
+		            show: true,
+		            sizeAdjust: 7.5
+		        },
+		        cursor: {
+		            show: false
+		        },
+		        series: [
+          { label: 'Systolic' },
+          { label: 'Diastolic' },
+                ],
+		        // Show the legend and put it outside the grid, but inside the
+		        // plot container, shrinking the grid to accomodate the legend.
+		        // A value of "outside" would not shrink the grid and allow
+		        // the legend to overflow the container. outsideGrid, outside,
+		        legend: {
+		            show: true,
+		            placement: 'inside'
+		        }
+
+		    });
+		};
+
+
+	return {
+		pageshow: pageshow
+	}
+}();
+
+
+
